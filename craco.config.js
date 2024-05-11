ef9a291c18bc47fdf7e16e0bebadb9ec7f5b5d8e
Amit Raikwar <managerarbusiness@gmail.com>
2024-05-11 23:18:44 +0530

    [Theme-Provider]: Adding theme support - Added theme provider using the chakra ui. - Added chakra ui in the project.

--- craco.config.js
+++ craco.config.js
@@ -4,6 +4,8 @@ module.exports = {
   webpack: {
     alias: {
       '@localization': path.resolve(__dirname, './src/localization'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@providers': path.resolve(__dirname, './src/providers'),
       '@store': path.resolve(__dirname, './src/store'),
       '@store/slice': path.resolve(__dirname, './src/store/slice'),
       '@selectors': path.resolve(__dirname, './src/store/selectors'),
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { ThemeProvider } from '@providers';
 
 function App() {
   const { t } = useTranslation();
   return (
-    <React.StrictMode>
-      <div>{t('App')}</div>
-    </React.StrictMode>
+    <ThemeProvider>
+      <React.StrictMode>
+        <div>{t('App')}</div>
+      </React.StrictMode>
+    </ThemeProvider>
   );
 }
 
